ecd6d32da289b25236850551cc75fd4718b1c131
Mika Senghaas <mail@mikasenghaas.de>
2025-03-13 18:35:11 +0000

    First working iroh send/recv with .wait()

--- test/test_iroh.py
+++ test/test_iroh.py
@@ -1,204 +1,207 @@
 import os
 import time
 import pytest
-from iroh_py import create_connector, create_sender, create_receiver
+import asyncio
+from iroh_py import create_sender, create_receiver
 from multiprocessing import Process, Queue, Pipe, Event
 from multiprocessing.connection import Connection
 
-@pytest.mark.skip(reason="Legacy test")
-def test_bi():
-    def run_node(conn: Connection):
-        print(f"running node in {os.getpid()} (p: {os.getppid()})")
-        node = create_connector()
-        node_id = node.get_node_id()
-        conn.send(node_id)
-        time.sleep(1)
-        print(f"listening on: {node_id}")
-        peer_id = conn.recv()
-        node.connect(peer_id)
-        while not node.is_ready():
-            time.sleep(0.01)
-        print(f"connected to: {peer_id}")
-        print(f"send/ recv ready: {peer_id}")
-        node.shutdown()
-
-    print(f"running test on {os.getpid()}")
-    a, b = Pipe()
-    node1 = Process(target=run_node, args=(a,))
-    node2 = Process(target=run_node, args=(b,))
-    node1.start()
-    node2.start()
-    node1.join()
-    node2.join()
-
-@pytest.mark.skip(reason="This test is not working")
-def test_uni():
-    def run_receiver(q: Queue):
-        print(f"running receiver in {os.getpid()} (p: {os.getppid()})")
+@pytest.mark.skip(reason="Debugging")
+def test_async_sender_receiver():
+    async def run_receiver(q: Queue):
+        print(f"Running receiver in {os.getpid()} (p: {os.getppid()})")
         receiver = create_receiver()
         node_id = receiver.get_node_id()
         q.put(node_id)
-        print(f"listening on: {node_id}")
+        print(f"Listening on: {node_id}")
         while not receiver.is_ready():
             time.sleep(0.01)
-        print("receiver ready")
-        data = receiver.recv()
-        print(f"received: {data}")
+        print("Receiver ready")
+        data = await receiver.recv()
+        print(f"Received: {data}")
         assert data == b"hello"
         receiver.shutdown()
 
-    def run_sender(q: Queue):
-        print(f"running sender in {os.getpid()} (p: {os.getppid()})")
+    async def run_sender(q: Queue):
+        print(f"Running sender in {os.getpid()} (p: {os.getppid()})")
         sender = create_sender()
         peer_id = q.get()
-        time.sleep(1) # TODO: wait for something explicit here (receiver was not yet ready)
-        print(f"connecting to: {peer_id}")
+        time.sleep(1) # TODO
+        print(f"Connecting to: {peer_id}")
         sender.connect(peer_id)
         while not sender.is_ready():
             time.sleep(0.01)
-        print("sender ready")
-        sender.send(b"hello")
+        print("Sender ready")
+        await sender.send(b"hello")
         sender.shutdown()
 
-    print(f"running test on {os.getpid()}")
+    print(f"Running test on {os.getpid()}")
+
+    def run_async(func, q: Queue):
+        asyncio.run(func(q))
+
     queue = Queue()
-    receiver = Process(target=run_receiver, args=(queue,))
-    sender = Process(target=run_sender, args=(queue,))
+    receiver = Process(target=run_async, args=(run_receiver, queue,))
+    sender = Process(target=run_async, args=(run_sender, queue,))
     receiver.start()
     sender.start()
     receiver.join()
     sender.join()
 
-class IrohReceiverSender:
-    """IrohNode runs receiver and sender in separate processes"""
-    def __init__(self, send_queue: Queue, recv_queue: Queue, conn: Connection):
-        self.send_queue = send_queue
-        self.recv_queue = recv_queue
-        self.conn = conn
-        self.stop = Event()
+class Future:
+    def __init__(self):
+        self._ready = Event()
+        self._result = None
+        self._error = None
         
-        # Queues to communicate node id subprocesses and main process
-        self.send_id, self.recv_id = Queue(), Queue()
+    def set_result(self, result):
+        self._result = result
+        self._ready.set()
+        
+    def set_error(self, error):
+        self._error = error
+        self._ready.set()
+        
+    def wait(self):
+        self._ready.wait()
+        if self._error:
+            raise RuntimeError(self._error)
+        return self._result
 
+class IrohNode:
+    """IrohNode running receiver and sender in separate processes"""
+    def __init__(self):
+        # Queues to communicate data between subprocesses and main process
+        self.send_queue = Queue()
+        self.recv_queue = Queue()
+        self.id_queue = Queue()
+        
         # Start receiver and sender in separate processes
-        self.receiver = Process(target=self.run_receiver, args=(recv_queue, self.recv_id))
-        self.sender = Process(target=self.run_sender, args=(send_queue, self.send_id))
+        self.receiver = Process(target=self.run_async, args=(self.run_receiver, self.recv_queue, self.id_queue))
+        self.sender = Process(target=self.run_async, args=(self.run_sender, self.send_queue, self.id_queue))
         self.receiver.start()
         self.sender.start()
-        self.receiver_id = self.recv_id.get()
+        self.receiver_id = self.id_queue.get()
 
-    def run_receiver(self, recv_queue: Queue, recv_id: Queue):
-        print(f"Run receiver in {os.getpid()} (p: {os.getppid()})")
+    def run_async(self, func, q: Queue, conn: Connection):
+        asyncio.run(func(q, conn))
+
+    async def run_receiver(self, recv_queue: Queue, id_queue: Queue):
+        print(f"Running receiver (p={os.getpid()}, pp={os.getppid()})")
         # Create receiver
         receiver = create_receiver()
         node_id = receiver.get_node_id()
 
         # Send node id to parent process
-        recv_id.put(node_id)
-        print(f"receiver node id: {node_id} (p={os.getpid()}, pp={os.getppid()})")
+        id_queue.put(node_id)
+        print(f"Listening on: {node_id} (pp={os.getppid()})")
 
         # Wait for sender to connect
         while not receiver.is_ready():
             time.sleep(0.01)
-        print(f"receiver ready (p={os.getpid()}, pp={os.getppid()})")
+        print(f"Receiver ready (pp={os.getppid()})")
 
-        while True:
-            try:
-                # Check for stop signal
-                if self.stop.is_set():
-                    break
-                    
-                # Blocking receive
-                print(f"receiving data: (p={os.getpid()}, pp={os.getppid()})")
-                data = receiver.recv()
-                print(f"received data: {data} (p={os.getpid()}, pp={os.getppid()})")
-                if data:
-                    recv_queue.put(data)
-            except Exception as e:
-                print(f"Receiver error: {e}")
-                break
+        # Receive data
+        try:
+            while True:
+                recv_queue.get()  # Just a signal to receive
+                try:
+                    data = await receiver.recv()
+                    recv_queue.put(("data", data))  # Send back result
+                except Exception as e:
+                    recv_queue.put(("error", str(e)))  # Send back error
+        finally:
+            print(f"Shutting down receiver (pp={os.getppid()})")
+            receiver.shutdown()
 
-        print(f"receiver shutdown (p={os.getpid()}, pp={os.getppid()})")
-        receiver.shutdown()
-
-    def run_sender(self, send_queue: Queue, send_id: Queue):
-        print(f"run sender (p={os.getpid()}, pp={os.getppid()})")
-
+    async def run_sender(self, send_queue: Queue, id_queue: Queue):
+        print(f"Running sender (p={os.getpid()}, pp={os.getppid()})")
         # Create sender
         sender = create_sender()
-        peer_id = send_id.get()
-        print(f"got peer id: {peer_id} (p={os.getpid()}, pp={os.getppid()})")
-        time.sleep(1) # TODO: fix this hardcoded sleep
+        peer_id = id_queue.get()
+        print(f"Got peer id: {peer_id} (pp={os.getppid()})")
+        time.sleep(1) # TODO
         sender.connect(peer_id)
-        print(f"connecting to: {peer_id} (p={os.getpid()}, pp={os.getppid()})")
+        print(f"Connecting to: {peer_id} (pp={os.getppid()})")
         while not sender.is_ready():
             time.sleep(0.01)
-        print(f"sender ready (p={os.getpid()}, pp={os.getppid()})")
-        while True:
-            try:
-                # Check for shutdown signal
-                if self.stop.is_set() and send_queue.empty():
-                    break
-                    
-                # Non-blocking check for data to send
-                try:
-                    data = send_queue.get_nowait()
-                    print(f"sending data: {data} (p={os.getpid()}, pp={os.getppid()})")
-                    sender.send(data)
-                except:
-                    time.sleep(0.01)  # Prevent busy waiting
-            except Exception as e:
-                print(f"Sender error: {e}")
-                break
+        print(f"Sender ready (pp={os.getppid()})")
 
-        print(f"sender shutdown (p={os.getpid()}, pp={os.getppid()})")
-        sender.shutdown()
+        try:
+            while True:
+                msg_type, data = send_queue.get()  # Receive just the message type and data
+                if msg_type == "shutdown":
+                    break
+                try:
+                    await sender.send(data)
+                    send_queue.put(("ok", None))  # Signal success
+                except Exception as e:
+                    send_queue.put(("error", str(e)))  # Signal error
+        finally:
+            print(f"Shutting down sender (pp={os.getppid()})")
+            sender.shutdown()
 
     def get_node_id(self):
         return self.receiver_id
 
     def connect(self, peer_id: str):
-        self.send_id.put(peer_id)
+        self.id_queue.put(peer_id)
 
-    def send(self, data: bytes):
-        self.send_queue.put(data)
+    def send(self, data: bytes) -> Future:
+        future = Future()
+        def _handle_result():
+            self.send_queue.put(("send", data))
+            result_type, result = self.send_queue.get()
+            if result_type == "error":
+                future.set_error(result)
+            else:
+                future.set_result(None)
+        
+        # Run in current process
+        _handle_result()
+        return future
 
-    def recv(self):
-        return self.recv_queue.get()
+    def recv(self) -> Future:
+        future = Future()
+        def _handle_result():
+            self.recv_queue.put("recv")  # Signal we want to receive
+            result_type, result = self.recv_queue.get()
+            if result_type == "error":
+                future.set_error(result)
+            else:
+                future.set_result(result)
+        
+        # Run in current process
+        _handle_result()
+        return future
 
     def shutdown(self):
-        print(f"shutdown in {os.getpid()}")
-        self.stop.set()
+        print(f"Shutting down in {os.getpid()}")
+        self.send_queue.put(("shutdown", None, None))
         self.receiver.join()
         self.sender.join()
 
-def test_bi_with_process():
+def test_iroh_node():
     def run_node(conn: Connection, rank: int):
-        print(f"running node {rank} in {os.getpid()}")
-        send_queue = Queue()
-        recv_queue = Queue()
-        node = IrohReceiverSender(send_queue, recv_queue, conn)
+        print(f"Running node {rank} in {os.getpid()}")
+        node = IrohNode()
         node_id = node.get_node_id()
         conn.send(node_id)
         peer_id = conn.recv()
         node.connect(peer_id)
 
-        time.sleep(3)
+        time.sleep(5)
 
-        if rank == 0:
-            node.send(b"hello")
-            node.send(b"world")
-        else:
-            data = node.recv()
-            assert data == b"hello"
-            data = node.recv()
-            assert data == b"world"
-        print("calling node shutdown")
+        print(f"Node {rank} sending hello")
+        node.send(b"hello").wait()
+        data = node.recv().wait()
+        assert data == b"hello"
+
         node.shutdown()
 
-    a, b  = Pipe()
-    node1 = Process(target=run_node, args=(a, 0))
-    node2 = Process(target=run_node, args=(b, 1))
+    conn_a, conn_b  = Pipe()
+    node1 = Process(target=run_node, args=(conn_a, 0))
+    node2 = Process(target=run_node, args=(conn_b, 1))
     node1.start()
     node2.start()
     node1.join()
